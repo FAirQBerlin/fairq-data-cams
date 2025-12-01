4e06a07578676bfba741518c657acf16d8ff259a
Jenkins Daemon <brother-mfc@inwt-statistics.de>
2025-12-01 17:51:38 +0100

    2025.09.0

--- fairq_data_cams/db_connect.py
+++ fairq_data_cams/db_connect.py
@@ -25,7 +25,7 @@ def db_connect() -> Client:
         user=os.getenv("DB_USERNAME"),
         password=os.getenv("DB_PASSWORD"),
         secure=True,
-        verify=True,
+        verify=False,
         settings={"use_numpy": True},
     )
 
